aa06537d0321feec542299efa56b029ef5526e7d
iqbalkahini <iqbal.lazuardi07@gmail.com>
2026-03-24 20:28:07 +0700

    hapus NEXT_PUBLIC_SUPABASE_ANON_KEY

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -10,8 +10,7 @@ export async function createClient() {
   const cookieStore = await cookies();
   const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
   const supabaseKey =
-    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY ||
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+    process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY
 
   if (!supabaseUrl || !supabaseKey) {
     throw new Error("Supabase environment variables are not configured.");
