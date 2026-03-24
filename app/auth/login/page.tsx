78f6698efa8e90975672c289cc1ca5c708356aaf
iqbalkahini <iqbal.lazuardi07@gmail.com>
2026-03-24 20:37:00 +0700

    Update page.tsx

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -1,12 +1,14 @@
-
 import { LoginForm } from "@/components/login-form";
+import { Suspense } from "react";
 
 export default function Page() {
   return (
-    // <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
-    //   <div className="w-full max-w-sm">
-    //   </div>
-    // </div>
-    <LoginForm />
+    <Suspense fallback={
+      <div className="flex min-h-svh w-full items-center justify-center p-6 md:p-10 bg-gradient-to-br from-blue-50 to-indigo-100">
+        <div className="animate-pulse text-blue-600 font-medium">Memuat...</div>
+      </div>
+    }>
+      <LoginForm />
+    </Suspense>
   );
 }
