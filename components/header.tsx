3be9c91ee49faf3328b357dc0b6feba045546a07
iqbalkahini <iqbal.lazuardi07@gmail.com>
2026-03-24 20:44:48 +0700

    Update header.tsx

--- components/header.tsx
+++ components/header.tsx
@@ -37,7 +37,7 @@ export default function Header({ title, onMenuClick }: HeaderProps) {
             </div>
 
             <div className="flex items-center gap-3">
-                {/* Notifications */}
+                {/* Notificationss */}
                 <button className="relative p-2 text-gray-400 hover:text-gray-600 hover:bg-gray-100 rounded-xl transition-all duration-200">
                     <Bell size={18} />
                 </button>
